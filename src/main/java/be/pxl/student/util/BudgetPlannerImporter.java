1286f178455c5aad1554154e4858ca4381a215ea
CasperSchepersPXL <11800427@student.pxl.be>
2020-03-18 11:24:20 +0100

    commit2

--- src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
+++ src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
@@ -1,15 +1,9 @@
 package be.pxl.student.util;
-import be.pxl.student.entity.Account;
-import be.pxl.student.entity.Payment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.ArrayList;
-
 
 /**
  * Util class to import csv file
@@ -18,6 +12,7 @@ public class BudgetPlannerImporter {
 
     private static final Logger LOGGER = LogManager.getLogger(BudgetPlannerImporter.class);
     private PathMatcher csvMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.csv");
+    private AccountMapper accountMapper = new AccountMapper();
 
     public void importCsv(Path path) {
         if (!csvMatcher.matches(path)) {
@@ -30,25 +25,15 @@ public class BudgetPlannerImporter {
 
         try (BufferedReader reader = Files.newBufferedReader(path)) {
             String line = null;
+            reader.readLine();
+
             while ((line = reader.readLine()) != null) {
-
-
-
-
-                String[] lines = line.split(",");
-                Account account = new Account();
-                account.setIBAN(lines[1]);
-                account.setName(lines[0]);
-
-                Payment payment = new Payment(LocalDateTime.parse(lines[3]),Float.parseFloat(lines[4]),lines[5],lines[6]);
-                List<Payment> payments = new ArrayList<>();
-                payments.add(payment);
-                account.setPayments(payments);
-                System.out.println(account.toString());
-
-
-
-
+                try {
+                    LOGGER.debug(accountMapper.map(line));
+                }
+                catch (InvalidPaymentException e) {
+                    LOGGER.error("Error while mapping line: {}", e.getMessage());
+                }
             }
         } catch (IOException e) {
             LOGGER.fatal("An error occured while reading : {}", path);
